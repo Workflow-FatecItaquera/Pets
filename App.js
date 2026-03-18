7d3df9131bb52b3c229f53771bba5c7c2a7ee969
Joia <gustavo.sc.joia@gmail.com>
2026-03-18 20:26:05 -0300

    CONFIG: Adição de dependências e estrutura de pastas

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Home from './src/view/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Home/>
+    </SafeAreaView>
   );
 }
 
