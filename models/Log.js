fcc99ef321ba5ac503ba7e9205e6125bdd32d232
Joia <gustavo.sc.joia@gmail.com>
2026-06-02 17:20:26 -0300

    Notificações

--- models/Log.js
+++ models/Log.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const LogSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   action: String,
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
+  message: String
 });
 
 const Log = mongoose.model("Log", LogSchema);
